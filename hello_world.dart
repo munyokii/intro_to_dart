d8fbb9b621733e91b66746d807b4fcd8bdf6c770
munyokii <munyoki912@gmail.com>
2025-09-23 14:41:34 +0300

    Introduction to dart

---
+++ hello_world.dart
@@ -0,0 +1,15 @@
+void main() {
+  String wizardName = "Merlin";
+  int magicLevel = 95;
+  double spellPower = 65.5;
+  bool hasMagicStaff = true;
+
+
+  print("Hello $wizardName");
+  print("Your magic level is $magicLevel");
+  print('Spell Power: $spellPower');
+  print('Has Staff: $hasMagicStaff');
+
+  magicLevel += 5;
+  print('New Magic Level: $magicLevel');
+}
