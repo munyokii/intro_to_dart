fcff66a8e7a8c2152ede30a35b8f01cf2e78c91a
munyokii <munyoki912@gmail.com>
2025-09-27 18:20:07 +0300

    Booleans

---
+++ boolean.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Boolean code: true or false logic
+  bool isCodingFun = true;
+  bool lovesDebugging = false;
+
+  // 🧠 Logical spells
+  if(isCodingFun) {
+    print("🎉 Coding is fun!");
+  } else {
+    print("💡 Try a new language or project for more fun!");
+  }
+
+  if (lovesDebugging) {
+    print("🐛 Debugging is like solving a puzzle!");
+  } else {
+    print("🚀 Focus on writing bug-free code!");
+  }
+}
