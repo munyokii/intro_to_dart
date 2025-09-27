f8eccb92cb61da4063a1e2caaa92d2b5b10db772
munyokii <munyoki912@gmail.com>
2025-09-27 22:14:01 +0300

    Decision making statements

--- controlFlow/ifStatement.dart
+++ controlFlow/ifStatement.dart
@@ -1,8 +1,10 @@
 void main() {
-  var age = 10;
+  var age = 19;
 
   if (age > 18) {
     print("🗳️ Mariam is a voter in Kenya!");
+  } else if (age == 18) {
+    print("🎉 Mariam is just old enough to vote in Kenya!");
   } else {
     print("👧 Mariam is still too young to vote.");
   }
