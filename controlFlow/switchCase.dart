4434762725e03d9caf371324722f17dd26ba7a14
munyokii <munyoki912@gmail.com>
2025-09-27 22:20:02 +0300

    Switch case statements

---
+++ controlFlow/switchCase.dart
@@ -0,0 +1,17 @@
+void main() {
+  int day = 1;
+
+  switch (day) {
+    case 1:
+      print("🌞 Monday: Let's code!");
+      break;
+    case 2:
+      print("🚀 Tuesday: Keep coding!");
+      break;
+    case 3:
+      print("🐪 Wednesday: Halfway through!");
+      break;
+    default:
+      print("🎉 Time for the weekend!");
+  }
+}
