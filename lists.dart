f159aedd29cbde9e84c22c11dc844464184ed4fe
munyokii <munyoki912@gmail.com>
2025-09-27 18:30:51 +0300

    Lists

---
+++ lists.dart
@@ -0,0 +1,11 @@
+void main() {
+  // using square brackets for listing
+  List myList = [1, 2, 5, "Walker"];
+  
+  // adding value to list 
+  myList.add(56);
+  myList.remove("Walker");
+  // removing a value
+  myList.remove(5);
+  print(myList);
+}
