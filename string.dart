d200a665e862e574704f9df40b5eb150df8708f9
munyokii <munyoki912@gmail.com>
2025-09-27 18:10:58 +0300

    Strings data type

---
+++ string.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Crafting string code
+  String coderName = "Dart Dev";
+  String favoriteTool = "VS Code";
+  String favoriteLanguage = "Dart";
+
+  // 🧙‍♀️ Combine strings using string interpolation (the power of `${}`!)
+  print("Hello, my name is $coderName, and I code in $favoriteTool using $favoriteLanguage");
+}
