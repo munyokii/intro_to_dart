0b33c6170b8c217ead5372f1875297627ad10a02
munyokii <munyoki912@gmail.com>
2025-10-10 16:57:47 +0300

    Function to find sum of two numbers

---
+++ functions/addingNumbers.dart
@@ -0,0 +1,10 @@
+// Function to find the sum of two numbers
+void addNum(int a, int b) {
+  int sum = a + b;
+  print('The sum of $a and $b is equal to: $sum');
+}
+
+void main() {
+  // Calling the function with two numbers
+  addNum(99, 5);
+}
