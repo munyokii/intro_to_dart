6259c36c33ee252b3a515337599d9a66acb1b29b
munyokii <munyoki912@gmail.com>
2025-10-11 15:45:58 +0300

    Anonymous function

---
+++ functions/anonymousFunction/printList.dart
@@ -0,0 +1,9 @@
+void main() {
+  // List of fruits 
+  var fruits = ['Melon', 'Apple', 'Lemon', 'Banana'];
+
+  // Using an anonymous function with forEach to print each fruit
+  fruits.forEach((fruit) {
+    print(fruit);
+  });
+}
---
+++ functions/anonymousFunction/printName.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Anonymous function assigned to the variable `printName`
+  var printName = () {
+    print('My name is Miriam');
+  };
+
+  // Calling the Function
+  printName();
+}
