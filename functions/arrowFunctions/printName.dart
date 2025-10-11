02a88da79eda43debd29aa58df7d006b1879b499
munyokii <munyoki912@gmail.com>
2025-10-11 15:56:02 +0300

    Printing name with arrow function

---
+++ functions/arrowFunctions/printName.dart
@@ -0,0 +1,9 @@
+// A simple Dart program using an arrow function
+void main() {
+  // Defining arrow Function to print name
+  var printName = (String name) => print("My name is: $name");
+
+  // Calling the Function
+  printName('Mary');
+
+}
