e530830cc8321181ba6b5cf40b6bd19443bd9b91
munyokii <munyoki912@gmail.com>
2025-10-21 11:27:14 +0300

    Enumeration utility

---
+++ dartUtilities/enums.dart
@@ -0,0 +1,11 @@
+enum dayOfClass { Mon, Tue, Wed, Thu, Fri }
+
+void main() {
+  dayOfClass dart = dayOfClass.Tue;
+
+  if (dart == dayOfClass.Tue) {
+    print('Let us learn dart programming');
+  } else {
+    print('Lets wait for Tue for dart programming');
+  }
+}
